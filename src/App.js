b5274ef0f4683ac6d0d0f3db2eef8abaf1e6eb95
geethasamynathan <geethasamynathan2011@gmail.com>
2024-11-21 12:53:53 +0530

    props added with data.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from "./logo.svg";
 import "./App.css";
-import ItemList from "./itemlist";
-import UserList from "./userlist";
+import conceptImg from "../src/assets/react-core-concepts.png";
+import Profile from "./components/avatar";
+import CoreConcept from "./components/corecocepts";
+import reactComponent from "./assets/react-components.jpeg";
+import { CORE_CONCEPTS } from "./data";
+
+const reactDescriptions = ["Fundamental", "Intermediate", "advanced"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[getRandomInt(2)];
+  return (
+    <header>
+      <main>
+        <section id="core-concepts">
+          <h2>Core concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              image={CORE_CONCEPTS[0].image}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              image={CORE_CONCEPTS[1].image}
+              description={CORE_CONCEPTS[1].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </header>
+  );
+}
 
 function App() {
-  const name = "Hexaware";
-  const sample = () => {
-    console.log("Button clicked");
-  };
   return (
     <div className="App">
-      <ItemList />
-      <UserList />
-      {/* <header className="App-header">
-        <h2> Hi All welcome {name}</h2>
-        <h3> {450 + 500}</h3>
-        <h3>{"sample" + 900} </h3>
-      </header>
-
-      <button onClick={sample}>click me</button> */}
+      <Header />
     </div>
   );
 }
